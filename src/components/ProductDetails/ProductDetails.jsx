60fdc20643dd3b2d3d4c1494c73aa74171082f06
Daniel Machado <needforock@gmail.com>
2023-06-07 16:37:21 -0400

    fix: styles and productdetails

--- src/components/ProductDetails/ProductDetails.jsx
+++ src/components/ProductDetails/ProductDetails.jsx
@@ -31,6 +31,7 @@ import { useTheme } from '@emotion/react'
 import ProductColors from '../ProductColors/ProductColors'
 import { BsCreditCard } from 'react-icons/bs'
 import { MdOutlineLocalShipping, MdStorefront } from 'react-icons/md'
+import { SliderComponent } from '../SliderCards/SliderCards'
 
 export default function ProductDetails ({ id }) {
   const toast = useToast()
@@ -49,7 +50,7 @@ export default function ProductDetails ({ id }) {
   }, [dispatch])
 
   const product = data ? data.find(product => product.id === id) : null
-  console.log(product)
+
   const handleAdd = qty => {
     addToCart(product, qty)
     toast({
@@ -74,172 +75,208 @@ export default function ProductDetails ({ id }) {
     <>
       <Cart />
       {product ? (
-        <Card
-          overflow='hidden'
-          variant='outline'
-          padding='1rem'
-          width={{ base: '100vw', md: '80vw' }}
-          margin='2rem auto'
-          border='none'
-        >
-          <Stack>
-            <CardBody
-              padding='0px'
-              display='flex'
-              flexDirection={{ base: 'column', sm: 'row' }}
-              justifyContent='space-between'
-              gap={10}
-            >
-              <Box
-                width={{ base: '100%', sm: '50%' }}
+        <>
+          <Card
+            overflow='hidden'
+            variant='outline'
+            padding='1rem'
+            width={{ base: '100vw', md: '75vw' }}
+            margin='2rem auto'
+            border='none'
+          >
+            <Stack>
+              <CardBody
+                padding='0px'
                 display='flex'
-                alignItems='center'
-                borderRadius={10}
-                border={{ base: 'none', md: 'solid 1px #EEEEEE' }}
+                flexDirection={{ base: 'column', sm: 'row' }}
+                justifyContent='space-between'
+                gap={10}
               >
-                <Image
-                  src={product.imageLink}
-                  alt='product image'
-                  borderRadius='lg'
-                  boxSize='lg'
-                  maxHeight='230px'
-                  maxWidth='200px'
-                  margin='0 auto'
-                  padding='0px'
-                />
-              </Box>
-              <Box
-                width={{ base: '100%', sm: '50%' }}
-                padding={{ base: '10px', sm: '20px' }}
-                borderRadius={10}
-                border={{ base: 'none', md: 'solid 1px #FAFAFA' }}
-                backgroundColor={{ base: '#FAFAFA', md: '#EEEEEE' }}
-              >
-                <Text py='2'>
-                  {product.brand && product.brand.toUpperCase()}
-                </Text>
-                <Heading size='md'>{product.name}</Heading>
-                <Box display='flex' mt='2' alignItems='center'>
-                  <Box>
-                    {Array(5)
-                      .fill('')
-                      .map((_, i) => (
-                        <StarIcon
-                          key={i}
-                          color={i < product.rating ? '#C42F6D' : 'gray.300'}
-                        />
-                      ))}
-                    {colors.length > 0 ? (
-                      <>
-                        <ProductColors colors={colors} />
-                      </>
-                    ) : null}
-                  </Box>
-                </Box>
-                <Text color='#1A1A1A' fontSize='2xl' fontWeight='bold'>
-                  ${product.price}
-                </Text>
-                <Flex marginTop='20px' justifyContent='space-between' gap={10}>
-                  <ProductCounter
-                    decreaseCounter={decreaseCounter}
-                    increaseCounter={increaseCounter}
-                    counter={counter}
+                <Box
+                  width={{ base: '100%', sm: '50%' }}
+                  display='flex'
+                  alignItems='center'
+                  borderRadius={10}
+                  border={{ base: 'none', md: 'solid 1px #EEEEEE' }}
+                >
+                  <Image
+                    src={product.imageLink}
+                    alt='product image'
+                    borderRadius='lg'
+                    boxSize='lg'
+                    maxHeight='230px'
+                    maxWidth='200px'
+                    margin='0 auto'
+                    padding='0px'
                   />
-                  <Button
-                    onClick={() => {
-                      handleAdd(counter)
-                    }}
-                    variant='solid'
-                    backgroundColor={backgroundColor}
-                    color={primaryColor}
-                    width='200px'
-                    height='44px'
-                  >
-                    Add to cart
-                  </Button>
-                </Flex>
-                <Flex justifyContent='space-around' mt={7}>
+                </Box>
+                <Box
+                  width={{ base: '100%', sm: '50%' }}
+                  padding={{ base: '10px', sm: '20px' }}
+                  borderRadius={10}
+                  border={{ base: 'none', md: 'solid 1px #FAFAFA' }}
+                  backgroundColor={{ base: '#FAFAFA', md: '#EEEEEE' }}
+                >
+                  <Text py='2'>
+                    {product.brand && product.brand.toUpperCase()}
+                  </Text>
+                  <Heading size='md'>{product.name}</Heading>
+                  <Box display='flex' mt='2' alignItems='center'>
+                    <Box>
+                      {Array(5)
+                        .fill('')
+                        .map((_, i) => (
+                          <StarIcon
+                            key={i}
+                            color={i < product.rating ? '#C42F6D' : 'gray.300'}
+                          />
+                        ))}
+                      {colors.length > 0 ? (
+                        <>
+                          <ProductColors colors={colors} />
+                        </>
+                      ) : null}
+                    </Box>
+                  </Box>
+                  <Text color='#1A1A1A' fontSize='2xl' fontWeight='bold'>
+                    ${product.price}
+                  </Text>
                   <Flex
-                    direction='column'
-                    alignItems='center'
-                    textAlign='center'
-                    padding='0 2px'
+                    marginTop='20px'
+                    justifyContent='space-between'
+                    gap={10}
                   >
-                    <BsCreditCard fontSize={35} />
-                    <Text fontSize={{ base: '14px', md: '16px' }}>
-                      Ver Medios de Pago
-                    </Text>
+                    <ProductCounter
+                      decreaseCounter={decreaseCounter}
+                      increaseCounter={increaseCounter}
+                      counter={counter}
+                    />
+                    <Button
+                      onClick={() => {
+                        handleAdd(counter)
+                      }}
+                      variant='solid'
+                      backgroundColor={backgroundColor}
+                      color={primaryColor}
+                      width='200px'
+                      height='44px'
+                    >
+                      Add to cart
+                    </Button>
                   </Flex>
-                  <Flex
-                    direction='column'
-                    alignItems='center'
-                    textAlign='center'
-                    borderRight='solid 1px #DADADA'
-                    borderLeft='solid 1px #DADADA'
-                    padding='0 2px'
-                  >
-                    <MdOutlineLocalShipping fontSize={35} />
-                    <Text fontSize={{ base: '14px', md: '16px' }}>
-                      Calcular costos de envio
-                    </Text>
+                  <Flex justifyContent='space-around' mt={7}>
+                    <Flex
+                      direction='column'
+                      alignItems='center'
+                      textAlign='center'
+                      padding='0 2px'
+                    >
+                      <BsCreditCard fontSize={35} />
+                      <Text fontSize={{ base: '14px', md: '16px' }} padding={2}>
+                        Payment Methods
+                      </Text>
+                    </Flex>
+                    <Flex
+                      direction='column'
+                      alignItems='center'
+                      textAlign='center'
+                      borderRight='solid 1px #DADADA'
+                      borderLeft='solid 1px #DADADA'
+                      padding='0 2px'
+                    >
+                      <MdOutlineLocalShipping fontSize={35} />
+                      <Text fontSize={{ base: '14px', md: '16px' }} padding={2}>
+                        Calculate Shipping
+                      </Text>
+                    </Flex>
+                    <Flex
+                      direction='column'
+                      alignItems='center'
+                      textAlign='center'
+                      padding='0 2px'
+                    >
+                      <MdStorefront fontSize={35} />
+                      <Text fontSize={{ base: '14px', md: '16px' }} padding={2}>
+                        Free PickUp
+                      </Text>
+                    </Flex>
                   </Flex>
-                  <Flex
-                    direction='column'
-                    alignItems='center'
-                    textAlign='center'
-                    padding='0 2px'
-                  >
-                    <MdStorefront fontSize={35} />
-                    <Text fontSize={{ base: '14px', md: '16px' }}>
-                      Retiro gratis en sucursal
-                    </Text>
-                  </Flex>
-                </Flex>
-              </Box>
-            </CardBody>
+                </Box>
+              </CardBody>
 
-            <CardFooter padding='0px'>
-              <Tabs isFitted variant='unstyled' defaultIndex={0} minHeight={400}>
-                <TabList width='30%'>
-                  <Tab>Details</Tab>
-                  <Tab>Reviews</Tab>
-                </TabList>
-                <TabIndicator
-                  mt='-1.5px'
-                  height='2px'
-                  backgroundColor={backgroundColor}
-                  borderRadius='1px'
-                />
-                <TabPanels>
-                  <TabPanel
-                    padding='10px'
-                    backgroundColor={{ base: '#FAFAFA', md: '#EEEEEE' }}
-                    borderRadius={5}
-                    marginTop='10px'
-                  >
-                    <Text>{product.description}</Text>
-                  </TabPanel>
-                  <TabPanel
-                    padding='10px'
-                    backgroundColor={{ base: '#FAFAFA', md: '#EEEEEE' }}
-                    borderRadius={5}
-                    marginTop='10px'
-                  >
-                    <p>
-                      Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                      Aperiam aut fuga incidunt ad ratione accusantium nam ipsa
-                      nisi veniam in aliquam magni amet, similique impedit.
-                      Assumenda repudiandae, doloribus deleniti nobis officia
-                      blanditiis quod minima nemo, distinctio aliquid dolorum.
-                      Laboriosam, odio recusandae? Asperiores molestiae beatae
-                      nesciunt, eius natus ad numquam. Nulla?
-                    </p>
-                  </TabPanel>
-                </TabPanels>
-              </Tabs>
-            </CardFooter>
-          </Stack>
-        </Card>
+              <CardFooter padding='0px'>
+                <Tabs
+                  isFitted
+                  variant='unstyled'
+                  defaultIndex={0}
+                  minHeight={150}
+                >
+                  <TabList width='30%'>
+                    <Tab>Details</Tab>
+                    <Tab>Reviews</Tab>
+                  </TabList>
+                  <TabIndicator
+                    mt='-1.5px'
+                    height='2px'
+                    backgroundColor={backgroundColor}
+                    borderRadius='1px'
+                  />
+                  <TabPanels>
+                    <TabPanel
+                      padding='10px'
+                      backgroundColor={{ base: '#FAFAFA', md: '#EEEEEE' }}
+                      borderRadius={5}
+                      marginTop='10px'
+                    >
+                      <Text>{product.description}</Text>
+                    </TabPanel>
+                    <TabPanel
+                      padding='10px'
+                      backgroundColor={{ base: '#FAFAFA', md: '#EEEEEE' }}
+                      borderRadius={5}
+                      marginTop='10px'
+                    >
+                      <p>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Aperiam aut fuga incidunt ad ratione accusantium
+                        nam ipsa nisi veniam in aliquam magni amet, similique
+                        impedit. Assumenda repudiandae, doloribus deleniti nobis
+                        officia blanditiis quod minima nemo, distinctio aliquid
+                        dolorum. Laboriosam, odio recusandae? Asperiores
+                        molestiae beatae nesciunt, eius natus ad numquam. Nulla?
+                      </p>
+                    </TabPanel>
+                  </TabPanels>
+                </Tabs>
+              </CardFooter>
+            </Stack>
+          </Card>
+          <Heading margin={{ base: '23px', md: '20px 100px' }}>
+            Related Products
+          </Heading>
+          <Box
+            margin='25px auto'
+            padding={5}
+            overflowX='scroll'
+            width='85vw'
+            css={{
+              '&::-webkit-scrollbar': {
+                width: '4px'
+              },
+              '&::-webkit-scrollbar-track': {
+                width: '6px'
+              },
+              '&::-webkit-scrollbar-thumb': {
+                background: '#EAEAEA',
+                borderRadius: '24px'
+              }
+            }}
+          >
+            {data && data.slice(0, 10) && (
+              <SliderComponent data={data.slice(0, 10)} />
+            )}
+          </Box>
+        </>
       ) : (
         <Flex height='70vh' justifyContent='center' alignItems='center'>
           <Spinner size='xl' margin='0 auto' />
